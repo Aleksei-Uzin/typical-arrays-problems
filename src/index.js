e5114c3d8472622e1c7191fe537fdec08bdf7671
Aleksei Uzin <aleksei.uzin@gmail.com>
2020-02-25 02:26:47 +0300

    feat: add new feature

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  array.sort(function (a, b) {
+    if (typeof a === "number" && typeof b === "number")
+      return a - b;
+    else
+      return 0;
+  });
+    return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  array.sort(function (a, b) {
+    if (typeof a === "number" && typeof b === "number")
+      return a - b;
+    else
+      return 0;
+  });
+  return array.pop();
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  var sum = 0;
+  for(var i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+  return ((sum / array.length) || 0);
 }
